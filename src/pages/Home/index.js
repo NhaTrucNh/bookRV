2e2f8bbeca7263f7eaf441aef7cb0891ac03b9eb
Nha Truc <86352840+NhaTrucNh@users.noreply.github.com>
2022-11-30 23:03:46 +0700

    home update
    Signed-off-by: Nha Truc <86352840+NhaTrucNh@users.noreply.github.com>

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,25 @@
+import classNames from 'classnames/bind';
+import styles from './Home.module.scss';
+import bluep4 from '../../asset/images/bluep4.jpg';
+
+const cx = classNames.bind(styles);
+
 function Home() {
-    return <h2>Home page</h2>;
+    return (
+        <section className={cx('wrapper')}>
+            <div className={cx('news-catalog')}>
+                <p>Mới xuất bản</p>
+                <hr width="100%" color="#E8E8E8" />
+            </div>
+            <div className={cx('book-publish')}>
+                <div className="frame">
+                    <a href="##">
+                        <img src={bluep4} alt="bluep4" />
+                    </a>
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default Home;
