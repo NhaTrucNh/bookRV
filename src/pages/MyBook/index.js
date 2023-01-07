fc92ac72aa0a78756ecd1896870d39d34c2b28e0
Nha Truc <86352840+NhaTrucNh@users.noreply.github.com>
2023-01-07 12:41:20 +0700

    muốn đọc to dự định đọc
    Signed-off-by: Nha Truc <86352840+NhaTrucNh@users.noreply.github.com>

--- src/pages/MyBook/index.js
+++ src/pages/MyBook/index.js
@@ -7,172 +7,172 @@ import styles from './MyBook.module.scss';
 const cx = classNames.bind(styles);
 
 export default class MyBook extends React.Component {
-  render() {
-    const handleChange = (value) => {
-      console.log(value);
-    };
-    return (
-      <>
-        <div className={cx('wrapper')}>
-          <div className={cx('Left')}>
-            <div className={cx('Shelf')}>
-              <h4>Tủ sách của tôi</h4>
-              <a href="##">
-                <p>
-                  Tất cả<span>(23)</span>
-                </p>
-              </a>
-              <a href="##">
-                <p>
-                  Muốn đọc<span>(10)</span>
-                </p>
-              </a>
-              <a href="##">
-                <p>
-                  Đang đọc<span>(5)</span>
-                </p>
-              </a>
-              <a href="##">
-                <p>
-                  Đã đọc<span>(6)</span>
-                </p>
-              </a>
-              <a href="##">
-                <p>
-                  Ngưng đọc<span>(2)</span>
-                </p>
-              </a>
-            </div>
-          </div>
-          <div className={cx('Right')}>
-            <div className={cx('BookTableStack')}>
-              <table>
-                <thead>
-                  <tr>
-                    <th className={cx('cover')}>Ảnh bìa</th>
-                    <th className={cx('title')}>Tiêu đề</th>
-                    <th className={cx('author')}>Tác giả</th>
-                    <th className={cx('avg')}>Điểm trung bình</th>
-                    <th className={cx('lineRate')}>Đánh giá</th>
-                    <th className={cx('shef')}>Tủ sách</th>
-                    <th className={cx('DateAdd')}>Ngày thêm</th>
-                    <th>Hành động</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>
-                      <img
-                        src="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/519namgWJTL._SY346_.jpg"
-                        alt=""
-                      />
-                    </td>
-                    <td className={cx('title')}>
-                      Harry Potter and the Order of the Phoenix (Harry Potter, #5)
-                    </td>
-                    <td className={cx('author')}>Rowling, J.K.</td>
-                    <td className={cx('avg')}>4.50</td>
-                    <td className={cx('lineRate')}>
-                      <Rate max={5} defaultValue={0} style={{ fontSize: 13 }} />
-                    </td>
-                    <td className={cx('shef')}>
-                      <Select
-                        labelInValue
-                        defaultValue={{
-                          value: 'Muốn đọc',
-                          label: 'Muốn đọc',
-                        }}
-                        style={{
-                          width: 120,
-                        }}
-                        onChange={handleChange}
-                        options={[
-                          {
-                            value: 'Muốn đọc',
-                            label: 'Muốn đọc',
-                          },
-                          {
-                            value: 'Đang đọc',
-                            label: 'Đang đọc',
-                          },
-                          {
-                            value: 'Đã đọc',
-                            label: 'Đã đọc',
-                          },
-                          {
-                            value: 'Ngưng đọc',
-                            label: 'Ngưng đọc',
-                          },
-                        ]}
-                      />
-                    </td>
-                    <td className={cx('DateAdd')}>Nov 29, 2022</td>
-                    <td>
-                      <DeleteOutlined />
-                    </td>
-                  </tr>
+    render() {
+        const handleChange = (value) => {
+            console.log(value);
+        };
+        return (
+            <>
+                <div className={cx('wrapper')}>
+                    <div className={cx('Left')}>
+                        <div className={cx('Shelf')}>
+                            <h4>Tủ sách của tôi</h4>
+                            <a href="##">
+                                <p>
+                                    Tất cả<span>(23)</span>
+                                </p>
+                            </a>
+                            <a href="##">
+                                <p>
+                                    Dự định đọc<span>(10)</span>
+                                </p>
+                            </a>
+                            <a href="##">
+                                <p>
+                                    Đang đọc<span>(5)</span>
+                                </p>
+                            </a>
+                            <a href="##">
+                                <p>
+                                    Đã đọc<span>(6)</span>
+                                </p>
+                            </a>
+                            <a href="##">
+                                <p>
+                                    Ngưng đọc<span>(2)</span>
+                                </p>
+                            </a>
+                        </div>
+                    </div>
+                    <div className={cx('Right')}>
+                        <div className={cx('BookTableStack')}>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th className={cx('cover')}>Ảnh bìa</th>
+                                        <th className={cx('title')}>Tiêu đề</th>
+                                        <th className={cx('author')}>Tác giả</th>
+                                        <th className={cx('avg')}>Điểm trung bình</th>
+                                        <th className={cx('lineRate')}>Đánh giá</th>
+                                        <th className={cx('shef')}>Tủ sách</th>
+                                        <th className={cx('DateAdd')}>Ngày thêm</th>
+                                        <th>Hành động</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            <img
+                                                src="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/519namgWJTL._SY346_.jpg"
+                                                alt=""
+                                            />
+                                        </td>
+                                        <td className={cx('title')}>
+                                            Harry Potter and the Order of the Phoenix (Harry Potter, #5)
+                                        </td>
+                                        <td className={cx('author')}>Rowling, J.K.</td>
+                                        <td className={cx('avg')}>4.50</td>
+                                        <td className={cx('lineRate')}>
+                                            <Rate max={5} defaultValue={0} style={{ fontSize: 13 }} />
+                                        </td>
+                                        <td className={cx('shef')}>
+                                            <Select
+                                                labelInValue
+                                                defaultValue={{
+                                                    value: 'Dự định đọc',
+                                                    label: 'Dự định đọc',
+                                                }}
+                                                style={{
+                                                    width: 120,
+                                                }}
+                                                onChange={handleChange}
+                                                options={[
+                                                    {
+                                                        value: 'Dự định đọc',
+                                                        label: 'Dự định đọc',
+                                                    },
+                                                    {
+                                                        value: 'Đang đọc',
+                                                        label: 'Đang đọc',
+                                                    },
+                                                    {
+                                                        value: 'Đã đọc',
+                                                        label: 'Đã đọc',
+                                                    },
+                                                    {
+                                                        value: 'Ngưng đọc',
+                                                        label: 'Ngưng đọc',
+                                                    },
+                                                ]}
+                                            />
+                                        </td>
+                                        <td className={cx('DateAdd')}>Nov 29, 2022</td>
+                                        <td>
+                                            <DeleteOutlined />
+                                        </td>
+                                    </tr>
 
-                  <tr>
-                    <td>
-                      <img
-                        src="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/519namgWJTL._SY346_.jpg"
-                        alt=""
-                      />
-                    </td>
-                    <td className={cx('title')}>
-                      Harry Potter and the Order of the Phoenix (Harry Potter, #5)
-                    </td>
-                    <td className={cx('author')}>Rowling, J.K.</td>
-                    <td className={cx('avg')}>4.50</td>
-                    <td className={cx('lineRate')}>
-                      <Rate max={5} defaultValue={0} style={{ fontSize: 13 }} />
-                    </td>
-                    <td className={cx('shef')}>
-                      <Select
-                        labelInValue
-                        defaultValue={{
-                          value: 'Muốn đọc',
-                          label: 'Muốn đọc',
-                        }}
-                        style={{
-                          width: 120,
-                        }}
-                        onChange={handleChange}
-                        options={[
-                          {
-                            value: 'Muốn đọc',
-                            label: 'Muốn đọc',
-                          },
-                          {
-                            value: 'Đang đọc',
-                            label: 'Đang đọc',
-                          },
-                          {
-                            value: 'Đã đọc',
-                            label: 'Đã đọc',
-                          },
-                          {
-                            value: 'Ngưng đọc',
-                            label: 'Ngưng đọc',
-                          },
-                        ]}
-                      />
-                    </td>
-                    <td className={cx('DateAdd')}>Nov 29, 2022</td>
-                    <td>
-                      <DeleteOutlined />
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+                                    <tr>
+                                        <td>
+                                            <img
+                                                src="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/519namgWJTL._SY346_.jpg"
+                                                alt=""
+                                            />
+                                        </td>
+                                        <td className={cx('title')}>
+                                            Harry Potter and the Order of the Phoenix (Harry Potter, #5)
+                                        </td>
+                                        <td className={cx('author')}>Rowling, J.K.</td>
+                                        <td className={cx('avg')}>4.50</td>
+                                        <td className={cx('lineRate')}>
+                                            <Rate max={5} defaultValue={0} style={{ fontSize: 13 }} />
+                                        </td>
+                                        <td className={cx('shef')}>
+                                            <Select
+                                                labelInValue
+                                                defaultValue={{
+                                                    value: 'Dự định đọc',
+                                                    label: 'Dự định đọc',
+                                                }}
+                                                style={{
+                                                    width: 120,
+                                                }}
+                                                onChange={handleChange}
+                                                options={[
+                                                    {
+                                                        value: 'Dự định đọc',
+                                                        label: 'Dự định đọc',
+                                                    },
+                                                    {
+                                                        value: 'Đang đọc',
+                                                        label: 'Đang đọc',
+                                                    },
+                                                    {
+                                                        value: 'Đã đọc',
+                                                        label: 'Đã đọc',
+                                                    },
+                                                    {
+                                                        value: 'Ngưng đọc',
+                                                        label: 'Ngưng đọc',
+                                                    },
+                                                ]}
+                                            />
+                                        </td>
+                                        <td className={cx('DateAdd')}>Nov 29, 2022</td>
+                                        <td>
+                                            <DeleteOutlined />
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
 
-            <div className={cx('Pagina')}>
-              <Pagination defaultCurrent={1} total={50} />
-            </div>
-          </div>
-        </div>
-      </>
-    );
-  }
+                        <div className={cx('Pagina')}>
+                            <Pagination defaultCurrent={1} total={50} />
+                        </div>
+                    </div>
+                </div>
+            </>
+        );
+    }
 }
--- src/pages/Review/index.js
+++ src/pages/Review/index.js
@@ -10,132 +10,148 @@ import styles from './Review.module.scss';
 const cx = classNames.bind(styles);
 
 export default function Review() {
-  const { id } = useParams();
-  const navigate = useNavigate();
-  const { Option } = Select;
-  const { TextArea } = Input;
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const { Option } = Select;
+    const { TextArea } = Input;
 
-  const [book, setBook] = useState({});
-  const [user, setUser] = useState({});
-  const [rating, setRating] = useState(0);
-  const [shelf, setShelf] = useState('');
-  const [content, setContent] = useState('');
+    const [book, setBook] = useState({});
+    const [user, setUser] = useState({});
+    const [rating, setRating] = useState(0);
+    const [shelf, setShelf] = useState('');
+    const [content, setContent] = useState('');
 
-  useEffect(() => {
-    if (Cookies.get('token') && localStorage.getItem('user')) {
-      userApi.getSelf(Cookies.get('token')).then((response) => {
-        if (response?.data.code === 200) {
-          setUser(response.data.result);
+    useEffect(() => {
+        if (Cookies.get('token') && localStorage.getItem('user')) {
+            userApi
+                .getSelf(Cookies.get('token'))
+                .then((response) => {
+                    if (response?.data.code === 200) {
+                        setUser(response.data.result);
+                    }
+                })
+                .catch((error) => {
+                    const msg = error.response.data.message ? error.response.data.message : 'Verify Failed';
+                    toast.error(msg);
+                    Cookies.remove('token');
+                    localStorage.removeItem('user');
+                    navigate('/login');
+                });
+        } else {
+            toast.error('Bạn chưa đăng nhập');
+            Cookies.get('token') && Cookies.remove('token');
+            localStorage.getItem('user') && localStorage.removeItem('user');
+            navigate('/login');
         }
-      }).catch((error) => {
-        const msg = error.response.data.message ? error.response.data.message : 'Verify Failed';
-        toast.error(msg);
-        Cookies.remove('token');
-        localStorage.removeItem('user');
-        navigate("/login");
-      })
-    } else {
-      toast.error('Bạn chưa đăng nhập');
-      Cookies.get('token') && Cookies.remove('token');
-      localStorage.getItem('user') && localStorage.removeItem('user');
-      navigate("/login");
-    }
-  }, [navigate])
+    }, [navigate]);
 
-  useEffect(() => {
-    if (user.id && id) {
-      reviewApi.getReview(user.id, id, Cookies.get('token')).then((response) => {
-        if (response?.data.code === 200) {
-          setRating(response.data.result.rating);
-          setShelf(response.data.result.shelf);
-          setContent(response.data.result.content);
+    useEffect(() => {
+        if (user.id && id) {
+            reviewApi
+                .getReview(user.id, id, Cookies.get('token'))
+                .then((response) => {
+                    if (response?.data.code === 200) {
+                        setRating(response.data.result.rating);
+                        setShelf(response.data.result.shelf);
+                        setContent(response.data.result.content);
+                    }
+                })
+                .catch((error) => {
+                    const msg = error.response.data.message ? error.response.data.message : 'Verify Failed';
+                    toast.error(msg);
+                });
         }
-      }).catch((error) => {
-        const msg = error.response.data.message ? error.response.data.message : 'Verify Failed';
-        toast.error(msg);
-      })
-    }
-  }, [user, id])
+    }, [user, id]);
 
-  useEffect(() => {
-    if (id) {
-      bookApi.getBook(id).then((response) => {
-        if (response?.data.code === 200) {
-          setBook(response.data.result);
+    useEffect(() => {
+        if (id) {
+            bookApi
+                .getBook(id)
+                .then((response) => {
+                    if (response?.data.code === 200) {
+                        setBook(response.data.result);
+                    }
+                })
+                .catch((error) => {
+                    const msg = error.response.data.message ? error.response.data.message : 'Verify Failed';
+                    toast.error(msg);
+                });
         }
-      }).catch((error) => {
-        const msg = error.response.data.message ? error.response.data.message : 'Verify Failed';
-        toast.error(msg);
-      })
-    }
-  }, [id])
+    }, [id]);
 
-  const handleSubmit = () => {
-    if (user.id && id) {
-      reviewApi.postReview(user.id, id, rating, shelf, content, Cookies.get('token')).then((response) => {
-        if (response?.data.code === 200) {
-          toast.success('Đánh giá thành công');
-          navigate(`/book/${id}`);
+    const handleSubmit = () => {
+        if (user.id && id) {
+            reviewApi
+                .postReview(user.id, id, rating, shelf, content, Cookies.get('token'))
+                .then((response) => {
+                    if (response?.data.code === 200) {
+                        toast.success('Đánh giá thành công');
+                        navigate(`/book/${id}`);
+                    }
+                })
+                .catch((error) => {
+                    const msg = error.response.data.message ? error.response.data.message : 'Verify Failed';
+                    toast.error(msg);
+                });
         }
-      }).catch((error) => {
-        const msg = error.response.data.message ? error.response.data.message : 'Verify Failed';
-        toast.error(msg);
-      })
-    }
-  }
+    };
 
-  return (
-    <>
-      <div className={cx('wrapper')}>
-        <div className={cx('bookinfo')}>
-          <div className={cx('cover')}>
-            <img
-              src={book.cover}
-              alt=""
-            />
-          </div>
-          <div className={cx('info')}>
-            <div>
-              <div className={cx('title')}>{book.title}</div>
-              <div className={cx('author')}>
-                <span>bởi </span>{book.author}
-              </div>
+    return (
+        <>
+            <div className={cx('wrapper')}>
+                <div className={cx('bookinfo')}>
+                    <div className={cx('cover')}>
+                        <img src={book.cover} alt="" />
+                    </div>
+                    <div className={cx('info')}>
+                        <div>
+                            <div className={cx('title')}>{book.title}</div>
+                            <div className={cx('author')}>
+                                <span>bởi </span>
+                                {book.author}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr />
+                <div className={cx('rating')}>
+                    <div className={cx('rate')}>
+                        <div className={cx('name')}>Đánh giá: </div>
+                        <Rate allowHalf defaultValue={rating} onChange={(value) => setRating(value)} />
+                    </div>
+                    <div className={cx('shelf')}>
+                        <div className={cx('name')}>Tủ sách: </div>
+                        <Select
+                            placeholder="Chọn tủ sách"
+                            style={{ width: 200 }}
+                            value={shelf}
+                            onChange={(value) => setShelf(value)}
+                        >
+                            <Option value="wantread">Dự định đọc</Option>
+                            <Option value="reading">Đang đọc</Option>
+                            <Option value="read">Đã đọc</Option>
+                            <Option value="drop">Ngưng đọc</Option>
+                        </Select>
+                    </div>
+                    <div className={cx('review')}>
+                        <div className={cx('name')}>Đánh giá: </div>
+                        <TextArea
+                            placeholder="Nhập cảm nhận của bạn vào đây"
+                            style={{
+                                height: 200,
+                                marginBottom: 24,
+                            }}
+                            value={content}
+                            onChange={(e) => setContent(e.target.value)}
+                        />
+                    </div>
+                    <div className={cx('post')}>
+                        <Button type="primary" onClick={handleSubmit}>
+                            Đăng
+                        </Button>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-        <hr />
-        <div className={cx('rating')}>
-          <div className={cx('rate')}>
-            <div className={cx('name')}>Đánh giá: </div>
-            <Rate allowHalf defaultValue={rating} onChange={(value) => setRating(value)} />
-          </div>
-          <div className={cx('shelf')}>
-            <div className={cx('name')}>Tủ sách: </div>
-            <Select placeholder="Chọn tủ sách" style={{ width: 200 }} value={shelf} onChange={(value) => setShelf(value)} >
-              <Option value="wantread">Muốn đọc</Option>
-              <Option value="reading">Đang đọc</Option>
-              <Option value="read">Đã đọc</Option>
-              <Option value="drop">Ngưng đọc</Option>
-            </Select>
-          </div>
-          <div className={cx('review')}>
-            <div className={cx('name')}>Đánh giá: </div>
-            <TextArea
-              placeholder="Nhập cảm nhận của bạn vào đây"
-              style={{
-                height: 200,
-                marginBottom: 24,
-              }}
-              value={content}
-              onChange={(e) => setContent(e.target.value)}
-            />
-          </div>
-          <div className={cx('post')}>
-            <Button type="primary" onClick={handleSubmit}>Đăng</Button>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-
+        </>
+    );
 }
