dbd5c924db51e6dcad6d516719333f140a5cf1fe
Nha Truc <86352840+NhaTrucNh@users.noreply.github.com>
2023-01-28 21:38:24 +0700

    Copy Paste Mod
    Signed-off-by: Nha Truc <86352840+NhaTrucNh@users.noreply.github.com>

--- src/pages/UserManagementMod/index.js
+++ src/pages/UserManagementMod/index.js
@@ -1,59 +1,117 @@
 import {
   CheckCircleOutlined,
-  EditOutlined,
   ExclamationCircleFilled,
   SearchOutlined,
   StopOutlined,
 } from '@ant-design/icons';
 import { Button, Modal, Pagination, Tag } from 'antd';
 import classNames from 'classnames/bind';
+import Cookies from 'js-cookie';
+import { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
+import { adminApi } from '~/api/api';
+import { DateConverter } from '~/helper/helper';
 import styles from './UserManagementMod.module.scss';
 
 const cx = classNames.bind(styles);
+const genderMap = {
+  male: 'Nam',
+  female: 'Nữ',
+  unknown: 'Không rõ',
+};
+const token = Cookies.get('token');
+
 export default function UserManagementMod() {
   const { confirm } = Modal;
+  const [users, setUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+  const [displayUsers, setDisplayUsers] = useState([]);
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+  const [query, setQuery] = useState('');
 
-  const showAddModConfirm = () => {
-    confirm({
-      title: 'Bạn có muốn thêm người dùng này thành điều phối viên không?',
-      icon: <ExclamationCircleFilled />,
-      content: 'Nhấn "Ok" để thêm',
-      onOk() {
-        return new Promise((resolve, reject) => {
-          setTimeout(Math.random() > 0.5 ? resolve : reject, 1000);
-        }).catch(() => console.log('Oops errors!'));
-      },
-      onCancel() {},
-    });
-  };
+  useEffect(() => {
+    adminApi
+      .getAllUsers(token)
+      .then((res) => {
+        if (res.data.result) {
+          setUsers(res.data.result);
+          setFilteredUsers(res.data.result);
+        }
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-  const showBannedConfirm = () => {
+  useEffect(() => {
+    filteredUsers && setDisplayUsers(filteredUsers.slice((page - 1) * pageSize, page * pageSize));
+  }, [filteredUsers, page, pageSize]);
+
+  useEffect(() => {
+    if (users.length > 0)
+      setFilteredUsers(
+        users.filter(
+          (user) =>
+            user.name?.includes(query) ||
+            user.email?.includes(query) ||
+            user.phoneNumber?.includes(query) ||
+            user.dateOfBirth?.includes(query) ||
+            genderMap[user.gender].includes(query),
+        ),
+      );
+  }, [users, query]);
+
+  const showDisableConfirm = (user) => {
     confirm({
       title: 'Bạn có muốn cấm người dùng này không?',
       icon: <ExclamationCircleFilled />,
       content: 'Nhấn "Ok" để khoá quyền hạn người dùng ',
       onOk() {
-        return new Promise((resolve, reject) => {
-          setTimeout(Math.random() > 0.5 ? resolve : reject, 1000);
-        }).catch(() => console.log('Oops errors!'));
+        adminApi
+          .disableUser(user.id, token)
+          .then((res) => {
+            if (res.data.code === 200) {
+              // const index = users.findIndex((userElement) => userElement.id === user.id);
+              const newUsers = users.map((userElement) => (userElement.id === user.id ? res.data.result : userElement));
+              console.log(newUsers);
+              setUsers(newUsers);
+              return toast.success(res.data.message);
+            } else return toast.error('Đã có lỗi xảy ra');
+          })
+          .catch((err) => {
+            const msg = err.response.data.message ? err.response.data.message : 'Đã có lỗi xảy ra';
+            return toast.error(msg);
+          });
       },
       onCancel() {},
     });
   };
 
-  const showUnbannedConfirm = () => {
+  const showEnableConfirm = (user) => {
     confirm({
       title: 'Bạn có muốn bỏ cấm người dùng này không?',
       icon: <ExclamationCircleFilled />,
       content: 'Nhấn "Ok" để khôi phục quyền hạn người dùng',
       onOk() {
-        return new Promise((resolve, reject) => {
-          setTimeout(Math.random() > 0.5 ? resolve : reject, 1000);
-        }).catch(() => console.log('Oops errors!'));
+        adminApi
+          .enableUser(user.id, token)
+          .then((res) => {
+            if (res.data.code === 200) {
+              // const index = users.findIndex((userElement) => userElement.id === user.id);
+              const newUsers = users.map((userElement) => (userElement.id === user.id ? res.data.result : userElement));
+              setUsers(newUsers);
+              return toast.success(res.data.message);
+            } else return toast.error('Đã có lỗi xảy ra');
+          })
+          .catch((err) => {
+            const msg = err.response.data.message ? err.response.data.message : 'Đã có lỗi xảy ra';
+            return toast.error(msg);
+          });
       },
       onCancel() {},
     });
   };
+
+  if (!users.length > 0) return <div>Loading...</div>;
 
   return (
     <>
@@ -64,135 +122,71 @@ export default function UserManagementMod() {
             <p>Quản lý tài khoản người dùng</p>
           </div>
           <div className={cx('search')}>
-            <form onSubmit="event.preventDefault();" role="search">
+          <form onSubmit={(e) => e.preventDefault()} role="search">
               <label htmlFor="search">Tìm kiếm</label>
-              <input id="search" type="search" placeholder="Nhập vào đây..." autoFocus required />
+              <input
+                id="search"
+                type="search"
+                placeholder="Nhập vào đây..."
+                autoFocus
+                required
+                onChange={(e) => setQuery(e.target.value)}
+              />
               <button type="submit">
                 <SearchOutlined style={{ fontSize: '16px', color: '#fff' }} />
               </button>
             </form>
           </div>
           <table className={cx('userList')}>
-            <tr>
-              <th width="15%">Tên</th>
-              <th width="20%">Email</th>
-              <th width="14%">Số điện thoại</th>
-              <th width="11%">Giới tính</th>
-              <th width="12%">Năm sinh</th>
-              <th width="15%">Trạng thái</th>
-              <th width="13%"></th>
-            </tr>
-            <tr>
-              <td>Nhã Trúc</td>
-              <td>heisia1412@gmail.com</td>
-              <td>0384673572</td>
-              <td>Nữ</td>
-              <td>27/2/1998</td>
-              <td>
-                <Tag color="green">Active</Tag>
-              </td>
-              <td className={cx('centerAlign')}>
-                <div>
-                  <Button onClick={showAddModConfirm} type="link" icon={<EditOutlined />} />
-                </div>
-                <div>
-                  <Button onClick={showBannedConfirm} type="link" icon={<StopOutlined />} danger />
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td>Nhã Trúc</td>
-              <td>heisia1412@gmail.com</td>
-              <td>0384673572</td>
-              <td>Nữ</td>
-              <td>27/2/1998</td>
-              <td>
-                <Tag color="red">Banned</Tag>
-              </td>
-              <td className={cx('centerAlign')}>
-                <div>
-                  <Button disabled onClick={showAddModConfirm} type="link" icon={<EditOutlined />} />
-                </div>
-                <div>
-                  <Button onClick={showUnbannedConfirm} type="link" icon={<CheckCircleOutlined />} />
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td>Nhã Trúc</td>
-              <td>heisia1412@gmail.com</td>
-              <td>0384673572</td>
-              <td>Nữ</td>
-              <td>27/2/1998</td>
-              <td>
-                <Tag color="green">Active</Tag>
-              </td>
-              <td className={cx('centerAlign')}>
-                <div>
-                  <Button onClick={showAddModConfirm} type="link" icon={<EditOutlined />} />
-                </div>
-                <div>
-                  <Button onClick={showBannedConfirm} type="link" icon={<StopOutlined />} danger />
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td>Nhã Trúc</td>
-              <td>heisia1412@gmail.com</td>
-              <td>0384673572</td>
-              <td>Nữ</td>
-              <td>27/2/1998</td>
-              <td>
-                <Tag color="green">Active</Tag>
-              </td>
-              <td className={cx('centerAlign')}>
-                <div>
-                  <Button onClick={showAddModConfirm} type="link" icon={<EditOutlined />} />
-                </div>
-                <div>
-                  <Button onClick={showBannedConfirm} type="link" icon={<StopOutlined />} danger />
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td>Nhã Trúc</td>
-              <td>heisia1412@gmail.com</td>
-              <td>0384673572</td>
-              <td>Nữ</td>
-              <td>27/2/1998</td>
-              <td>
-                <Tag color="green">Active</Tag>
-              </td>
-              <td className={cx('centerAlign')}>
-                <div>
-                  <Button onClick={showAddModConfirm} type="link" icon={<EditOutlined />} />
-                </div>
-                <div>
-                  <Button onClick={showBannedConfirm} type="link" icon={<StopOutlined />} danger />
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td>Nhã Trúc</td>
-              <td>heisia1412@gmail.com</td>
-              <td>0384673572</td>
-              <td>Nữ</td>
-              <td>27/2/1998</td>
-              <td>
-                <Tag color="green">Active</Tag>
-              </td>
-              <td className={cx('centerAlign')}>
-                <div>
-                  <Button onClick={showAddModConfirm} type="link" icon={<EditOutlined />} />
-                </div>
-                <div>
-                  <Button onClick={showBannedConfirm} type="link" icon={<StopOutlined />} danger />
-                </div>
-              </td>
-            </tr>
+          <thead>
+              <tr>
+                <th width="15%">Tên</th>
+                <th width="20%">Email</th>
+                <th width="14%">Số điện thoại</th>
+                <th width="11%">Giới tính</th>
+                <th width="12%">Ngày sinh</th>
+                <th width="15%">Trạng thái</th>
+                <th width="13%">Hành động</th>
+              </tr>
+            </thead>
+            <tbody>
+              {displayUsers?.map((user, index) => (
+                <tr key={index}>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.phoneNumber}</td>
+                  <td>{genderMap[user.gender]}</td>
+                  <td>{user.dateOfBirth && DateConverter(user.dateOfBirth).dateOnly}</td>
+                  <td>
+                    <Tag color={user.accountStatus.status === 'active' ? 'green' : 'red'}>
+                      {user.accountStatus.status === 'active' ? 'Đang hoạt động' : 'Dừng hoạt động'}
+                    </Tag>
+                  </td>
+                  <td className={cx('centerAlign')}>
+                    <div>
+                      <Button
+                        onClick={() =>
+                          user.accountStatus.status === 'active' ? showDisableConfirm(user) : showEnableConfirm(user)
+                        }
+                        type="link"
+                        icon={user.accountStatus.status === 'active' ? <StopOutlined /> : <CheckCircleOutlined />}
+                        danger={user.accountStatus.status === 'active'}
+                      />
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
           </table>
           <div className={cx('pagin')}>
-            <Pagination defaultCurrent={1} total={50} />
+          <Pagination
+              total={users?.length}
+              defaultCurrent={page}
+              defaultPageSize={pageSize}
+              onChange={(value) => setPage(value)}
+              pageSizeOptions={[10, 20, 50]}
+              onShowSizeChange={(current, size) => setPageSize(size)}
+            />
           </div>
         </div>
       </div>
